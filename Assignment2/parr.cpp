5dfa47e69c14cbfe4ece49ad5b0a76449494385d
Jarred Parr <jparr721@gmail.com>
2017-09-15 14:44:16 -0400

    Finished project 2

---
+++ Assignment2/parr.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "MyStack.h"
+
+template <typename Object>
+void MyStack::push(const Object& obj) {
+    MyStack::elements.push_back(obj);
+}
+
+void MyStack::pop() {
+    MyStack::elements.pop_back();
+}
+
+template <typename Object>
+Object MyStack::top() const {
+    MyStack::elements.front();
+}
+
+bool MyStack::empty() const {
+    return MyStack::elements.size() >= 1;
+}
+
+int MyStack::GetSize() const {
+    return MyStack::elements.size();
+}
+
