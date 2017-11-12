d88e3383f49ec6f53667da219e2e726306be9f33
Jarred Parr <jparr721@gmail.com>
2017-11-12 16:24:01 -0500

    done

--- proj3/backup.sh
+++ proj3/backup.sh
@@ -2,25 +2,54 @@
 #Ask for admin password
 sudo -v
 
-target="" # Default to an empty target
+if [ -z "$1" ]
+then
+	echo "The usage of this program is: backup [options] targetFileList"
+	exit 1
+fi
 
+if [[ $1 != *'-'* ]]; then
+	file1=$1
+	file2=$2
+	file3=$3
+else
+	file1=$2
+	file2=$3
+	file3=$4
+fi
+
+function backup_files() {
+	if [[ -z `ls -a | grep .backup` ]]; then
+		echo "Making .backup..."
+		mkdir ~/.backup
+		cp -R `pwd`/{$1,$2,$3} ~/.backup
+	else
+		rm -rf ~/.backup
+		mkdir ~/.backup
+		cp -R `pwd`/{$1,$2,$3} ~/.backup
+	fi
+}
+
+backup_files $file1 $file2 $file3
 
 while getopts ":lnh" opt; do
 	case "${opt}" in
 		l)
-			echo "Listing files in .backup:" 1>&2
+			printf "Listing files in .backup:\n\n"
 			ls -al ../proj2
 			;;
 		n)
-			echo "There are `ls -1 | wc -l` files that take up `du -k ~/.backup | awk '{print $1"kb"}'` of space in .backup"
+			printf "There are `ls -1 | wc -l` files that take up `du -k ~/.backup | awk '{print $1"kb"}'` of space in .backup\n\n"
 			;;
 		h)
+			printf "\n\n"
 			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
 			echo "-n	Show the number of files in .backup and how much space they take up"
 			echo "-l	List files currently in -l"
+			echo "-h	Show help page"
 			;;
 		\?)
-			echo "Usage: cmd [-l] [-n] [--help]"
+			printf "backup: Invalid option: $1\n\n"
 			# fak off m8
 			exit 1
 			;;
