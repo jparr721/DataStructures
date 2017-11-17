234d22210582986a4f89daf6c88d86f3f0e9a751
Jarred Parr <jparr721@gmail.com>
2017-11-17 16:34:08 -0500

    Code cleanup

--- proj3/backup.sh
+++ proj3/backup.sh
@@ -3,9 +3,6 @@ regex="^-."
 ARGUMENTS=()
 FILES=()
 
-list_in_directory=`ls -al ~/.backup | awk '{print $9}'`
-number_of_files="There are`ls -1|wc -l` files that take up `du -skh ~/.backup` of space in .backup\n\n"
-
 function handle_flags() {
 	case ${ARGUMENTS[@]} in
 		-l)
@@ -20,6 +17,33 @@ function handle_flags() {
 			ls -al ~/.backup | awk '{print $9}'
 			;;
 		--help)
+			printf "\n\n"
+			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
+			echo "-n	Show the number of files in .backup and how much space they take up"
+			echo "-l	List files currently in -l"
+			echo "-h	Show help page"
+			;;
+		-ln--help | -nl--help | --help-ln | --help-nl)
+			echo Listing files in .backup: 
+			ls -al ~/.backup | awk '{print $9}'
+			printf "There are`ls -1|wc -l` files that take up `du -skh ~/.backup` of space in .backup\n\n"
+			printf "\n\n"
+			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
+			echo "-n	Show the number of files in .backup and how much space they take up"
+			echo "-l	List files currently in -l"
+			echo "-h	Show help page"
+			;;
+		-l--help | --help-l)
+			echo Listing files in .backup: 
+			ls -al ~/.backup | awk '{print $9}'
+			printf "\n\n"
+			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
+			echo "-n	Show the number of files in .backup and how much space they take up"
+			echo "-l	List files currently in -l"
+			echo "-h	Show help page"
+			;;
+		-n--help | --help-n)
+			printf "There are`ls -1|wc -l` files that take up `du -skh ~/.backup` of space in .backup\n\n"
 			printf "\n\n"
 			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
 			echo "-n	Show the number of files in .backup and how much space they take up"
@@ -53,9 +77,9 @@ do
 		FILES+="`pwd`/$input "
 	fi
 done
-if [ ${#FILES[@]} -eq 0] && [${#ARGUMENTS[@]} -eq 0] 
+if [ ${#FILES[@]} -eq 0 ] && [ ${#ARGUMENTS[@]} -eq 0 ] 
 then
-	echo "Usage: The usage of this command is: ./backup.sh [options] targetFileList"
+	echo "Error, no arguments. The usage of this command is: ./backup.sh [options] targetFileList"
 fi
 
 if [[ ${#FILES[@]} -eq 0 ]]; then
@@ -64,7 +88,7 @@ else
 	backup_files ${FILES[@]}
 fi
 if [[ ${#ARGUMENTS[@]} -eq 0 ]]; then
-	echo "Yeah"
+	echo ""
 else
 	handle_flags ${ARGUMENTS[@]}
 fi
