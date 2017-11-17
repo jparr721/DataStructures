47a3f69e6fccd71b08dfd1d29cea4d8814bda266
Jarred Parr <jparr721@gmail.com>
2017-11-17 16:03:45 -0500

    Error checking done, ready for show and tell *wink*

--- proj3/backup.sh
+++ proj3/backup.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 regex="^-."
-args=""
 ARGUMENTS=()
 FILES=()
+
+list_in_directory=`ls -al ~/.backup | awk '{print $9}'`
+number_of_files="There are`ls -1|wc -l` files that take up `du -skh ~/.backup` of space in .backup\n\n"
+
+function handle_flags() {
+	case ${ARGUMENTS[@]} in
+		-l)
+			echo Listing files in .backup: 
+			ls -al ~/.backup | awk '{print $9}'
+			;;
+		-n)
+			printf "There are`ls -1|wc -l` files that take up `du -skh ~/.backup` of space in .backup\n\n"
+			;;
+		-ln | -nl)
+			printf "There are`ls -1|wc -l` files that take up `du -skh ~/.backup` of space in .backup\n\n"
+			ls -al ~/.backup | awk '{print $9}'
+			;;
+		--help)
+			printf "\n\n"
+			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
+			echo "-n	Show the number of files in .backup and how much space they take up"
+			echo "-l	List files currently in -l"
+			echo "-h	Show help page"
+			;;
+		esac
+
+}
 
 function clean() {
 	echo "Cleaning old .backup files"
@@ -27,36 +53,19 @@ do
 		FILES+="`pwd`/$input "
 	fi
 done
+if [ ${#FILES[@]} -eq 0] && [${#ARGUMENTS[@]} -eq 0] 
+then
+	echo "Usage: The usage of this command is: ./backup.sh [options] targetFileList"
+fi
+
 if [[ ${#FILES[@]} -eq 0 ]]; then
-	echo "No files, bitch"
+	echo ""
 else
 	backup_files ${FILES[@]}
 fi
+if [[ ${#ARGUMENTS[@]} -eq 0 ]]; then
+	echo "Yeah"
+else
+	handle_flags ${ARGUMENTS[@]}
+fi
 
-
-
-while getopts ":lnh" opt; do
-	case "${opt}" in
-		l)
-			printf "Listing files in .backup:\n"
-			ls -al ~/.backup | awk '{print $9}'
-			;;
-		n)
-			printf "There are `ls -1 | wc -l` files that take up `du -skh ~/.backup | awk '{print $1"b"}'` of space in .backup\n\n"
-			;;
-		h)
-			printf "\n\n"
-			echo "BACKUP(1)		Extended Command Manual		BACKUP(1)"
-			echo "-n	Show the number of files in .backup and how much space they take up"
-			echo "-l	List files currently in -l"
-			echo "-h	Show help page"
-			;;
-		\?)
-			printf "backup: Invalid option: $1\n\n"
-			# fak off m8
-			exit 1
-			;;
-
-	esac
-done
-shift $((OPTIND -1))
