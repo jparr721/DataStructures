6202700a3bd6cb7c723cf265c30b660754781422
Jarred Parr <jparr721@gmail.com>
2017-11-27 14:17:47 -0500

    Init project, build system set up

---
+++ Assignment5/parr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+const int INF = 1000000000;
+
+int main()
+{
+    // int n = 6;  //number of vertices
+
+    // vector<vector<int>> G = { {0,1,INF,4,4,INF},
+    //                           {INF,0,INF,2,INF,INF},
+    //                           {INF,INF,0,INF,INF,1},
+    //                           {INF,INF,2,0,3,INF},
+    //                           {INF,INF,INF,INF,0,3},
+    //                           {INF,INF,INF,INF,INF,0} };
+
+    return EXIT_SUCCESS;
+}
