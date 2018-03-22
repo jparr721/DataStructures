3f03f4600a7f3db25be683c27fc944bcba97f248
Jarred Parr <jparr721@gmail.com>
2018-03-22 10:11:35 -0400

    Two down 4 to go

---
+++ CTCI/Ch3StackAndQueue/MinStack.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class MinStack {
+    ArrayList<Integer> stack = new ArrayList<>();
+    ArrayList<Integer> minStack = new ArrayList<>();
+    int min = Integer.MAX_VALUE;
+
+    public void push(int data) {
+        if (data < min) {
+            min = data;
+            minStack.add(data);
+        }
+        stack.add(data);
+    }
+
+    public int peek() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public int pop() {
+        int poppedVal = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        if (poppedVal == min) {
+            minStack.remove(minStack.size() - 1);
+            min = minStack.size() - 1; // Assign the next in line for min value
+        }
+        return poppedVal;
+    }
+
+    public int min() {
+        return min;
+    }
+
+    public static void main(String[] args) {
+        MinStack ms = new MinStack();
+        ms.push(10);
+        ms.push(4);
+        ms.push(19);
+        ms.push(1);
+        ms.push(11);
+        System.out.println(ms.peek());
+        System.out.println(ms.min());
+    }
+}
