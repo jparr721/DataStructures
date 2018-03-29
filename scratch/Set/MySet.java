2e566e8a54de13728a89a939019b5bc6f11c524c
Jarred Parr <jparr721@gmail.com>
2018-03-29 10:34:01 -0400

    Finished more complex functionality

--- scratch/Set/MySet.java
+++ scratch/Set/MySet.java
@@ -1,4 +1,9 @@
 import java.util.*;
+/**
+ * This set implementation just does
+ * the more complex functionality since
+ * the arrayList does the same crap anyway
+ */
 
 public class MySet<T> {
     private ArrayList<T> setList;
@@ -28,6 +33,34 @@ public class MySet<T> {
         }
         return false;
     } 
+
+    public boolean removeAll(Collection<T> C) {
+        int numRemovals = setList.size();
+        for (T value : C) {
+            if (setList.remove(value)) {
+                numRemovals--;
+            }
+        }
+        if (numRemovals == 0) {
+            return true;
+        } 
+        return false;
+    }
+
+    public boolean retainAll(Collection<T> C) {
+        int matches = 0;
+        for (int i = 0; i < setList.size(); i++) {
+            if (!C.contains(setList.get(i))) {
+                setList.remove(i);
+            } else {
+                matches++;
+            }
+        }
+        if (matches == C.size()) {
+            return true;
+        }
+        return false;
+    }
 
     public void clear() {
         setList.clear();
