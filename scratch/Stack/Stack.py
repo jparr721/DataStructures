4f03adc35538e1ea19ac10f1abd363182fee9791
Jarred Parr <jparr721@gmail.com>
2018-04-05 10:29:01 -0400

    Unfinished

--- scratch/Stack/Stack.py
+++ scratch/Stack/Stack.py
@@ -1,2 +1,23 @@
 class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def empty(self):
+        return self.stack == []
+
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+
+    def pop(self):
+        return self.stack.pop()
+
+    def push(self, data):
+        return self.stack.push(data)
+
+    def search(self, value):
+        for i in range(len(self.stack) - 1):
+            if value == self.stack[i]
+                return i
+        return None 
+
 
