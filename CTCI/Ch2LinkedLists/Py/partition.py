cb41b00cc9f1acfec5ad15f29880016e9161b178
Jarred Parr <jparr721@gmail.com>
2018-08-02 09:26:41 -0400

    folder restructure and adding problems

--- CTCI/Ch2LinkedLists/Py/partition.py
+++ CTCI/Ch2LinkedLists/Py/partition.py
@@ -13,13 +13,14 @@ class Node(object):
         self.next_node = next_node
 
 class LinkedList(object):
-    head = None
+    def __init__(self, head=None):
+        self.head = head
 
     @property
-    def print(self):
+    def print_list(self):
         temp = self.head
         while temp is not None:
-            print(temp.data)
+            print('{} -->'.format(temp.data), end='')
             temp = temp.next_node
 
     def insert(self, data):
@@ -30,5 +31,56 @@ class LinkedList(object):
         new_node.next_node = self.head
         self.head = new_node
 
+    def find(self, item):
+        temp = self.head
+
+        while temp is not None:
+            if temp.data == item:
+                return True
+            temp = temp.next_node
+        return False
 
 
+def partition(linked_list, node):
+    """Partitions a linked list around a given node"""
+    small_list = LinkedList(None)
+    big_list = LinkedList(None)
+
+    if linked_list.find(node) is False:
+        return False
+
+    temp = linked_list.head
+
+    while temp is not None:
+        if temp.data < node:
+            small_list.insert(temp.data)
+        else:
+            big_list.insert(temp.data)
+        temp = temp.next_node
+
+    # print('=======================\n')
+    # small_list.print_list
+    # print('========================\n')
+    # big_list.print_list
+    # print('========================\n')
+    small_list.next_node = big_list
+    print('\n')
+    small_list.print_list
+    print('\n')
+    return small_list
+
+def main():
+    ll = LinkedList(None)
+    ll.insert(3)
+    ll.insert(5)
+    ll.insert(8)
+    ll.insert(5)
+    ll.insert(10)
+    ll.insert(2)
+    ll.insert(1)
+    ll.print_list
+    new_list = partition(ll, 5)
+    print('----------------------------')
+    new_list.print_list
+
+main()
