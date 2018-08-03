46c1beccb98b7f41da17fac3a952e43a560f814f
Jarred Parr <jparr721@gmail.com>
2018-08-03 14:01:52 -0400

    nothing to see here

---
+++ OddCode/createme.sql
@@ -0,0 +1,2 @@
+INSERT INTO account(id, email, status, first_name, last_name, client_primary_key, is_admin, is_saml_created, created_at, updated_at, client_id) values('B14plXIOZhM', 'jarredparr@justsift.com', 'ACTIVE', 'Jarred', 'Parr', 2476341, true, false, '2018-05-02 22:27:05.000000', '2018-06-06 18:03:47.000000', '5f801da2b9aa43c59ff0c724e71e534c');
+
