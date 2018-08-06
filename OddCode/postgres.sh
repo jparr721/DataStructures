960cc629c685677a01422f944286142f3f3a82ee
Jarred Parr <jparr721@gmail.com>
2018-08-06 07:42:09 -0400

    cleaning up a bit

--- OddCode/postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop $(cat process.txt) >/dev/null && docker rm $(cat process.txt) >/dev/null
-
-docker run --name devpostgres -p 5432:5432 -e DOCKER_HOST=127.0.0.1 -e POSTGRES_PASSWORD=lotus -e POSTGRES_DB=devel_people -v /home/lotus/Work/devdb/db-data:/var/lib/postgresql/data -d postgres:9.6 > process.txt
-
-
