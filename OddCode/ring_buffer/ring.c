bb64dc5b2f94847de41f50785c17e844d662155b
Jarred Parr <jparr721@gmail.com>
2019-02-08 09:40:12 -0500

    Update ring.c

--- OddCode/ring_buffer/ring.c
+++ OddCode/ring_buffer/ring.c
@@ -7,10 +7,12 @@ void init_ring_buffer() {
   init(&ra, 10);
 }
 
+// Pushes to an array regardless of available space
 void push(struct ring_buffer* rb, BuffNode bn) {
   append(rb->ray, bn);
 }
 
+// Pushes to an array if and only if space is available
 void offer(struct ring_buffer* rb, BuffNode bn) {
   if (!rb->full) {
     rb->tail = (rb->tail + 1) % rb->max;
@@ -21,29 +23,32 @@ void offer(struct ring_buffer* rb, BuffNode bn) {
   rb->full = (rb->head == rb->tail);
 }
 
+// Resets the ring buffer
 void buffer_reset(struct ring_buffer* rb) {
   assert(rb);
 
+  // TODO: Call array delete method
   rb->head = 0;
   rb->tail = 0;
   rb->full = false;
 }
 
+// Chcecks if the ring buffer is full
 bool buffer_full(struct ring_buffer* rb) {
   assert(rb);
 
-  remove_value(rb->ray, rb->head);
   return rb->full;
 }
 
+// Returns the current array size
 size_t buffer_size(struct ring_buffer* rb) {
   assert(rb);
 
   return rb->ray->size;
 }
 
+// Pops the first element off of the front of the queue
 BuffNode pop(RArray* ra) {
-  int len = sizeof(ra->array)/sizeof(ra->array[0]) - 1;
-
-  return ra->array[len];
+  remove_value(rb->ray, rb->head);
+  return ra->array[rb->head];
 }
