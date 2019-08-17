8650e5b22521b8f1bf93aa9961be6347719fde12
Jarred Parr <jparr721@gmail.com>
2019-08-17 16:55:10 -0600

    fix route between nodes

--- CTCI/Ch4TreesAndGraphs/py/route_between_nodes.py
+++ CTCI/Ch4TreesAndGraphs/py/route_between_nodes.py
@@ -4,18 +4,51 @@ in a directed graph
 """
 
 
-def between_two_nodes(graph, n1, n2):
-    print(graph[n2][n1])
-    return graph[n1][n2] == 1 or graph[n2][n1] == 1
+def route_between_nodes(adj, n1, n2):
+    if len(adj) == 0 or len(adj) == 1:
+        return False
+
+    history = [(n1, 0)]
+    visited = [n1]
+    current = n1
+
+    while history:
+        pos = history[-1][1]
+
+        if len(adj[current]) > pos:
+            current = adj[current][pos]
+
+        if current == n2:
+            return True
+
+        if current in visited:
+            # Get the last position
+            old_pos = history[-1][1]
+            if len(adj[current]) > old_pos:
+                # So, if we've visited this node, but there's another one
+                # then we just continue moving...
+                history[-1][1] += 1
+            else:
+                # Otherwise, ditch this node and keep moving
+                history.pop()
+        else:
+            visited.append(current)
+            history.append((current, 0))
+
+    return False
 
 
-def main():
-    adj = [[0, 1, 0, 0],
-           [0, 0, 1, 0],
-           [1, 0, 0, 0],
-           [0, 0, 1, 0]]
+if __name__ == "__main__":
+    adj = {
+        1: [2, 5, 6],
+        2: [5],
+        3: [4],
+        4: [3],
+        5: [6],
+        6: [],
+    }
 
-    print(between_two_nodes(adj, 1, 0))
-
-
-main()
+    print(route_between_nodes(adj, 1, 5))
+    print(route_between_nodes(adj, 1, 6))
+    print(route_between_nodes(adj, 1, 3))
+    print(route_between_nodes(adj, 5, 6))
