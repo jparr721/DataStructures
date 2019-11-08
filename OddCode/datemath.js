cc10133afad9449988623da8b688559a67819779
Jarred Parr <jparr721@gmail.com>
2019-11-08 13:00:14 -0700

    Create datemath.js

---
+++ OddCode/datemath.js
@@ -0,0 +1,13 @@
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+const a = new Date("2017-01-01"),
+    b = new Date("2017-07-25"),
+    difference = dateDiffInDays(a, b);
