5c38fa6675120d2f68e5e4dd37d9dc3446317777
Jarred Parr <jparr721@gmail.com>
2020-02-08 19:45:49 -0700

    holy shit it finally works!!

--- OddCode/brace_expansion.cc
+++ OddCode/brace_expansion.cc
@@ -4,6 +4,17 @@
 #include <unordered_map>
 #include <vector>
 
+std::string p_vec(const std::vector<std::string> &input) {
+  std::ostringstream ret;
+
+  for (const auto val : input) {
+    ret << val;
+    ret << ", ";
+  }
+
+  return ret.str();
+}
+
 std::unordered_map<int, std::vector<std::string>>
 ParseParameters(const std::string &expand_string) {
   std::unordered_map<int, std::vector<std::string>> parsed;
@@ -41,6 +52,10 @@ ParseParameters(const std::string &expand_string) {
     if (expand_string[i] == '{')
       continue;
     if (expand_string[i] == '}') {
+      grouping.push_back(word.str());
+      word.str("");
+      word.clear();
+
       parsed.insert(std::make_pair(current_parsed_index, grouping));
       grouping.clear();
       ++current_parsed_index;
@@ -50,7 +65,7 @@ ParseParameters(const std::string &expand_string) {
 
         std::ostringstream delimeter;
         if (index == std::string::npos) {
-          for (size_t j = i; j < (expand_string.length() - (i + 1)); ++j) {
+          for (size_t j = i + 1; j < (expand_string.length() - (i + 1)); ++j) {
             delimeter << expand_string[j];
           }
           parsed.insert(std::make_pair(
@@ -59,7 +74,7 @@ ParseParameters(const std::string &expand_string) {
           break;
         }
 
-        for (size_t j = i; j < index; ++j) {
+        for (size_t j = i + 1; j < index; ++j) {
           delimeter << expand_string[j];
         }
 
@@ -86,22 +101,66 @@ ParseParameters(const std::string &expand_string) {
   return parsed;
 }
 
-void BraceExpansion(const std::string &expand_string) {}
-
-void p_vec(const std::vector<std::string> &input) {
-  std::cout << "vector: ";
-  for (const auto &val : input) {
-    std::cout << val << std::endl;
+std::vector<std::string> AppendSubgroup(
+    int i, const std::string &leading_value,
+    const std::unordered_map<int, std::vector<std::string>> &parsed) {
+  const auto can_go_deeper = parsed.find(i + 1);
+  if (can_go_deeper != parsed.end()) {
+    const auto val = parsed.at(i);
+    for (const auto sub : val) {
+      AppendSubgroup(i + 1, sub, parsed);
+    }
   }
+
+  std::vector<std::string> ret;
+  const auto v = parsed.at(i);
+
+  for (const std::string value : v) {
+    ret.push_back(leading_value + value);
+  }
+
+  return ret;
+}
+
+std::vector<std::vector<std::string>> BraceExpansion(
+    const std::unordered_map<int, std::vector<std::string>> &parsed) {
+  const auto zero_index = parsed.find(0);
+  std::string prefix = "";
+  if (zero_index != parsed.end()) {
+    prefix = parsed.at(0)[0];
+  }
+
+  auto starter = parsed.at(1);
+  std::vector<std::vector<std::string>> ret;
+  for (const auto subval : starter) {
+    if (prefix != "") {
+      ret.push_back(std::vector<std::string>{prefix + subval});
+    } else {
+      ret.push_back(std::vector<std::string>{subval});
+    }
+  }
+
+  for (size_t i = 2; i < parsed.size(); ++i) {
+    const auto values = parsed.at(i);
+
+    for (const auto value : values) {
+      for (size_t j = 0; j < ret.size(); ++j) {
+        ret[j][0] = ret[j][0] + value;
+      }
+    }
+  }
+
+  return ret;
 }
 
 int main() {
-  const std::string expand_string = "{1,2,3}-{4,5,6}";
+  const std::string expand_string = "-------{1,2,3}-{4,5,6}-{7,8,9}";
   const std::unordered_map<int, std::vector<std::string>> parsed =
       ParseParameters(expand_string);
 
-  for (const auto p : parsed) {
-    std::cout << p.first << " " << std::endl;
-    p_vec(p.second);
+  const auto out = BraceExpansion(parsed);
+
+  for (const auto vec : out) {
+    std::cout << p_vec(vec) << std::endl;
   }
 }
